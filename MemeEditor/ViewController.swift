730f0a4947ed3eb443e0f25abb84da283e9e77bf
Jay Shah <jayshah@yahoo.com>
2018-01-18 20:02:36 -0800

    Some comments added

--- MemeEditor/ViewController.swift
+++ MemeEditor/ViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 	
+	// MARK: IBOutlets for UI Elements
 	@IBOutlet weak var memeImage: UIImageView!
 	@IBOutlet weak var shareButton: UIBarButtonItem!
 	@IBOutlet weak var cameraButton: UIBarButtonItem!
 	@IBOutlet weak var topTextField: UITextField!
 	@IBOutlet weak var bottomTextField: UITextField!
-
+	@IBOutlet weak var navBar: UINavigationBar!
+	@IBOutlet weak var toolBar: UIToolbar!
+	
+	//Struct to store meme when using save function and future functionality
 	struct Meme {
 		let topText: String
 		let bottomText: String
@@ -23,14 +27,20 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 		let memedImage: UIImage
 	}
 	
+	//Optional instance of Meme struct to hold meme after "saving"
 	var meme: Meme?
+	//Variable to keep track of active text field to be used to slide up current view
+	var activeTextField: UITextField?
 	
+	//Define the text attributes for the meme text. black stroke, white text, Helvetica font
+	//-3.0 stroke widht needed for the stroke to apply to the exterior so the font color will work
 	let memeTextAttributes:[String:Any] = [
 		NSAttributedStringKey.strokeColor.rawValue: UIColor.black,
 		NSAttributedStringKey.foregroundColor.rawValue: UIColor.white,
 		NSAttributedStringKey.font.rawValue: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
 		NSAttributedStringKey.strokeWidth.rawValue: -3.0]
 	
+	// MARK: UIImagePickerControllerDelegate functions
 	func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
 		dismiss(animated: true, completion: nil)
 	}
@@ -40,16 +50,20 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 			memeImage.image = image
 			shareButton.isEnabled = true
 		}
-		dismiss(animated: true, completion: nil)
+		dismiss(animated: true, completion: {
+			UIViewController.attemptRotationToDeviceOrientation()
+		})
 	}
 	
+	// MARK: UITextFieldDelegate functions
 	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
 		textField.resignFirstResponder()
 		return true
 	}
 	
 	func textFieldDidBeginEditing(_ textField: UITextField) {
-		if textField.text == "TOP" || textField.text == "BOTTOM" {
+		activeTextField = textField
+		if (textField == topTextField && textField.text == "TOP") || (textField == bottomTextField && textField.text == "BOTTOM") {
 			textField.text = ""
 		}
 	}
@@ -83,11 +97,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 	}
 	
 	@objc func keyboardWillShow(_ notification: Notification) {
-		view.frame.origin.y -= getKeyboardHeight(notification)
+		if activeTextField == bottomTextField {
+			view.frame.origin.y -= getKeyboardHeight(notification)
+		}
 	}
 	
 	@objc func keyboardWillHide(_ notifcation: Notification) {
-		view.frame.origin.y = 0
+			view.frame.origin.y = 0
 	}
 	
 	func getKeyboardHeight(_ notification: Notification) -> CGFloat {
@@ -106,6 +122,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 		NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
 	}
 	
+	// MARK: IBAction functions
+	//Pick image from library
 	@IBAction func pickImage(_ sender: Any) {
 		let pickerController = UIImagePickerController()
 		pickerController.delegate = self
@@ -113,6 +131,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 		present(pickerController, animated: true, completion: nil)
 	}
 	
+	//Pick image from Camera
 	@IBAction func pickImageFromCamera(_ sender: Any) {
 		let pickerController = UIImagePickerController()
 		pickerController.delegate = self
@@ -120,25 +139,48 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 		present(pickerController, animated: true, completion: nil)
 	}
 	
+	//Share button pressed
 	@IBAction func shareMeme(_ sender: Any) {
 		let memedImage = generateMemedImage()
-		let images: [UIImage] = [memedImage]
-		let shareViewController = UIActivityViewController(activityItems: images, applicationActivities: nil)
+		let shareViewController = UIActivityViewController(activityItems: [memedImage], applicationActivities: nil)
 		present(shareViewController, animated: true) {
 			self.meme = Meme(topText: self.topTextField.text!, bottomText: self.bottomTextField.text!, originalImage: self.memeImage.image!, memedImage: memedImage)
 		}
 	}
 	
+	//Cancel button pressed
 	@IBAction func cancelMeme(_ sender: Any) {
 		memeImage.image = nil
 		shareButton.isEnabled = false
 		topTextField.text = "TOP"
 		bottomTextField.text = "BOTTOM"
+		
+		//dismiss keyboard
+		topTextField.resignFirstResponder()
+		bottomTextField.resignFirstResponder()
+		
+		//delete meme?
 	}
 	
 	func generateMemedImage() -> UIImage {
 		
 		// TODO: Hide toolbar and navbar
+//		let navHeight = navBar.frame.size.height
+//		let toolHeight = toolBar.frame.size.height
+//		navBar.frame.size.height = 0
+//		toolBar.frame.size.height = 0
+		navBar.isHidden = true
+		toolBar.isHidden = true
+//		self.view.updateConstraintsIfNeeded()
+//		self.view.setNeedsLayout()
+//		self.view.layoutIfNeeded()
+		memeImage.frame.origin.y -= navBar.frame.size.height
+		memeImage.frame.size.height += navBar.frame.size.height + toolBar.frame.size.height
+		topTextField.frame.origin.y -= navBar.frame.size.height
+		bottomTextField.frame.origin.y += toolBar.frame.size.height
+//		self.view.frame.origin.y -= navBar.frame.size.height
+//		self.view.frame.size.height -= toolBar.frame.size.height
+//		memeImage.frame.size.height += navBar.frame.size.height + toolBar.frame.size.height
 		
 		// Render view to an image
 		UIGraphicsBeginImageContext(self.view.frame.size)
@@ -147,6 +189,20 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 		UIGraphicsEndImageContext()
 		
 		// TODO: Show toolbar and navbar
+//		navBar.frame.size.height = navHeight
+//		toolBar.frame.size.height = toolHeight
+		navBar.isHidden = false
+		toolBar.isHidden = false
+//		self.view.updateConstraintsIfNeeded()
+//		self.view.setNeedsLayout()
+//		self.view.layoutIfNeeded()
+		memeImage.frame.origin.y += navBar.frame.size.height
+		memeImage.frame.size.height -= navBar.frame.size.height + toolBar.frame.size.height
+		topTextField.frame.origin.y += navBar.frame.size.height
+		bottomTextField.frame.origin.y -= toolBar.frame.size.height
+ //		self.view.frame.origin.y += navBar.frame.size.height
+//		self.view.frame.size.height -= navBar.frame.size.height + toolBar.frame.size.height
+//		memeImage.frame.size.height -= navBar.frame.size.height + toolBar.frame.size.height
 		
 		return memedImage
 	}
